b38f22530bb980f67896a19fc933ec7d123e4c7c
Warda Bounab <wardabounab@MacBook-Pro-de-Warda.local>
2024-08-03 09:19:46 +0100

    fix(cors)

--- index.js
+++ index.js
@@ -10,37 +10,26 @@ import router from "./routers/views.js";
 const app = express();
 
 const corsOptions = {
-  origin: 'https://pokemon-khys.onrender.com', // Remplacez par l'URL de votre site
+  origin: 'https://pokemon-khys.onrender.com',
   optionsSuccessStatus: 200,
 };
-
-// Utilisation de CORS avec les options définies
 app.use(cors(corsOptions));
-
-// Gestion des requêtes preflight pour toutes les routes
-app.options('*', cors(corsOptions));
-
 app.use(bodyParser.json());
 app.use(limiter);
 app.use("/public", express.static("./views"));
 
-// Connexion à la base de données
 DBConnect();
-
-// Configuration du moteur de vues
 app.set("view engine", "pug");
 app.set("views", "./views");
 
-// Utilisation des routers
 app.use("/pokimon", pokimonRouters);
 app.use("/public", router);
 
-// Définir une route simple
 app.get("/", (req, res) => {
-  res.send("index"); // Assurez-vous que 'index' existe dans le dossier 'views'
+  res.send("index"); 
 });
 
-// Démarrage du serveur
+
 app.listen(process.env.API_PORT, () => {
   console.log(`Server is running on port`, process.env.API_PORT);
 });
